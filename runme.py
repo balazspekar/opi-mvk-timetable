9184d553b7616cd1db84369f31b1683fb1da7e03
Balazs Pekar <peba0706@gmail.com>
2017-03-20 19:41:51 +0100

    fine tuning the script here and there

--- runme.py
+++ runme.py
@@ -1,17 +1,21 @@
-# from lcd import LCD
+from lcd import LCD
 from schedule import Schedule
 import time
+import datetime
 
 
-def prettify(schedule):
-    result = ""
+def prettify(schedule, destination):
+    result = destination + ":"
     schedule.update_remaining_seconds()
     departures = schedule.remaining_seconds
-
     counter = 0
     for departure in departures:
-        if counter < 100:
-            result += str(departure) + " "
+        print(departure)
+        if counter < 5:
+            in_minute = departure // 60
+            if in_minute < 10 and in_minute >= 0:
+                    result += "0"
+            result += str(in_minute) + " "
             counter += 1
         else:
             break
@@ -24,8 +28,20 @@ def prettify(schedule):
 if __name__ == '__main__':
     screen = LCD()
     schedule_downtown = Schedule("514")
-    # schedule_suburb = Schedule("513")
-
-    while True:
-        print(prettify(schedule_downtown))
-        time.sleep(3)
+    schedule_suburb = Schedule("513")
+    requests = 0
+    try:
+        while True:
+            prettified_1 = prettify(schedule_downtown, "B")
+            prettified_2 = prettify(schedule_suburb, "D")
+            requests += 1
+            screen.lcd_string(prettified_1, 1)
+            screen.lcd_string(prettified_2, 2)
+            time.sleep(5)
+            current_dt = datetime.datetime.now()
+            screen.lcd_string(str(current_dt.year) + "/" + str(current_dt.month) + "/" + str(current_dt.day), 1)
+            screen.lcd_string(str(current_dt.hour) + ":" + str(current_dt.minute) + " [" + str(requests) + "]", 2)
+            time.sleep(2)
+    finally:
+        screen.lcd_string("Exited", 1)
+        screen.lcd_string("", 2)
